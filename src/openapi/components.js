9c0c7b0715f4ad911f2ee21e6a1f0f9d88d92804
MartinCasciana <martincasciana@DESKTOP-EGUA9BL.localdomain>
2020-07-22 12:18:54 -0300

    feat(project): add objects schemas

--- src/openapi/components.js
+++ src/openapi/components.js
@@ -44,6 +44,174 @@ module.exports = {
                 avatar: {type: 'string'}
             }
         },
+        Country: {
+            type: 'object',
+            properties: {
+                id: {
+                    type: 'string',
+                    format: 'uuid',
+                    nullable: true
+                },
+                name: {
+                    description: 'Name.',
+                    type: 'string'
+                },
+                code: {
+                    description: 'Code.',
+                    type: 'string'
+                }
+            }
+        },
+        Company: {
+            type: 'object',
+            properties: {
+                id: {
+                    type: 'string',
+                    format: 'uuid',
+                    nullable: true
+                },
+                industry: {
+                    description: 'Industry.',
+                    type: 'string'
+                },
+                name: {
+                    description: 'Name.',
+                    type: 'string'
+                },
+                symbol: {
+                    description: 'Symbol.',
+                    type: 'string'
+                },
+                sector: {
+                    description: 'Sector.',
+                    type: 'string'
+                },
+                street_address: {
+                    description: 'Street_address.',
+                    type: 'string'
+                },
+                city: {
+                    description: 'City.',
+                    type: 'string'
+                },
+                state: {
+                    description: 'State.',
+                    type: 'string'
+                },
+                phone: {
+                    description: 'Phone.',
+                    type: 'string'
+                },
+                email: {
+                    description: 'Email.',
+                    type: 'string'
+                },
+                market: {
+                    description: 'Market.',
+                    type: 'string'
+                },
+                market_cap: {
+                    description: 'Market_cap.',
+                    type: 'string'
+                }
+            }
+        },
+        Employe: {
+            type: 'object',
+            properties: {
+                id: {
+                    type: 'string',
+                    format: 'uuid',
+                    nullable: true
+                },
+                name: {
+                    description: 'Name.',
+                    type: 'string'
+                },
+                gender: {
+                    description: 'Gender.',
+                    type: 'string'
+                },
+                age: {
+                    description: 'Age.',
+                    type: 'integer'
+                },
+                email: {
+                    description: 'Email.',
+                    type: 'string'
+                },
+                phone: {
+                    description: 'Phone.',
+                    type: 'string'
+                },
+                company_name: {
+                    description: 'Company_name.',
+                    type: 'string'
+                },
+                sector: {
+                    description: 'Sector.',
+                    type: 'string'
+                }
+            }
+        },
+        Student: {
+            type: 'object',
+            properties: {
+                id: {
+                    type: 'string',
+                    format: 'uuid',
+                    nullable: true
+                },
+                name: {
+                    description: 'Name.',
+                    type: 'string'
+                },
+                gender: {
+                    description: 'Gender.',
+                    type: 'string'
+                },
+                age: {
+                    description: 'Age.',
+                    type: 'integer'
+                },
+                email: {
+                    description: 'Email.',
+                    type: 'string'
+                },
+                phone: {
+                    description: 'Phone.',
+                    type: 'string'
+                },
+                country: {
+                    description: 'Country.',
+                    type: 'string'
+                },
+                city: {
+                    description: 'City.',
+                    type: 'string'
+                },
+                university: {
+                    description: 'University.',
+                    type: 'string'
+                },
+                job_title: {
+                    description: 'Job_title.',
+                    type: 'string'
+                }
+            }
+        },
+        parameters: {
+            Path: {
+                name: 'id',
+                in: 'path',
+                required: true,
+                schema: {
+                    type: 'string',
+                    format: 'uuid'
+                },
+                description: 'ID'
+            }
+        },
         Error: {
             type: 'object',
             required: [
