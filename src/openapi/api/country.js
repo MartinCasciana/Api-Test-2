8b99c74427be88d8a68d8b762a155dddf596773f
MartinCasciana <martincasciana@DESKTOP-EGUA9BL.localdomain>
2020-08-20 12:08:08 -0300

    feat(project): add country openapi

---
+++ src/openapi/api/country.js
@@ -0,0 +1,148 @@
+module.exports = {
+    '/api/country': {//si queremos poner un id se pone '/api/country/{id}'
+        get: {
+            security: [],
+            summary: 'List Countries',
+            parameters: [
+                {
+                    in: 'query',
+                    name: 'code',
+                    schema: {
+                        type: 'string',
+                        pattern: '^[A-Z]{2}$'
+                    },
+                    description: 'codigo del pais solicitado'
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'login success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {
+                                    countries: {
+                                        type: 'array',
+                                        items: {$ref: '#/components/schemas/Country'}
+                                    },
+                                    total: {type: 'integer'},
+                                    limit: {type: 'integer'}
+                                }
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        },
+
+        post: {
+            security: [],
+            summary: 'crate country',
+            requestBody:{
+                description: 'new country',
+                content: {'application/json': {schema: {$ref: '#/components/schemas/Country'}}}
+            },
+            responses: {
+                200: {
+                    description: 'login success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+
+            }
+        }
+    },
+
+    '/api/countries/{id}': {
+        put: {
+            security: [],
+            summary: 'user id',
+            parameters: [{$ref: '#/components/parameters/Path'}],
+            requestBody: {
+                description: 'modify',
+                content: {'application/json': {schema: {$ref: '#/components/schemas/Country'}}}
+            },
+            responses: {
+                200: {
+                    description: 'login success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+
+            }
+        },
+
+        delete: {
+            security: [],
+            summary: 'user id',
+            parameters: [{$ref: '#/components/parameters/Path'}],
+            responses: {
+                200: {
+                    description: 'login success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+
+            }
+        },
+
+        get: {
+            security: [],
+            summary: 'user id',
+            parameters: [{$ref: '#/components/parameters/Path'}],
+            responses: {
+                200:{
+                    description: 'one countrie',
+                    content:{
+                        'application/json':{
+                            schema:{
+                                type: 'object',
+                                properties:{}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        }
+    }
+};
